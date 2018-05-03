d4da9b92494729b46ba93299341ff432c3070ed4
ozpinwang <wmlwonder@gmail.com>
2018-05-03 21:37:33 +0800

    旋转数组，done

--- 数组/旋转数组/main.py
+++ 数组/旋转数组/main.py
@@ -1,4 +1,6 @@
 # coding: utf-8
+import math
+
 
 class Solution:
     def rotate(self, nums, k):
@@ -7,9 +9,43 @@ class Solution:
         :type k: int
         :rtype: void Do not return anything, modify nums in-place instead.
         """
-        buf = -1;
-        for i in range(0, k):
-            pass
+        n = len(nums)
+        k = k % n
+
+        begin = -1
+        buf = -1
+        i=0 #记录交换次数
+
+        begin = 0
+        buf = nums[0]
+        nextP = 0
+        while True:
+            nextP = nextP + k
+            if nextP > n-1:
+                nextP = nextP % n
+
+            if i == n-1:
+                nums[begin] = buf
+                break
+
+            if nextP!=begin:
+                # 正常情况，执行交换
+                tmp = buf
+                buf = nums[nextP]
+                nums[nextP] = tmp
+                i = i+1
+                # print("nums:{}, buf:{}".format(nums, buf))
+            # 有可能在结束前跳成死循环，判断该情况
+            else:
+                nums[begin] = buf
+                i = i+1
+                begin += 1
+                buf = nums[begin]
+                nextP = begin
 
 if __name__=='__main__':
     s = Solution()
+    nums = [1,2,3]
+    k = 2
+    s.rotate(nums=nums, k=k)
+    print("res:{}".format(nums))
