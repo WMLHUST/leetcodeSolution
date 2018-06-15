a7639bdae8c249aa8f99df3c145a6d5c1e8adaa3
ozpinwang <wmlwonder@gmail.com>
2018-06-15 22:01:37 +0800

    add 3

---
+++ 数组/最长连续序列.py
@@ -0,0 +1,42 @@
+class Solution:
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if nums is None or len(nums)==0:
+            return 0
+
+        vals = {}
+        for num in nums:
+            vals[num] = 1
+
+        for num in nums:
+            if vals[num] > 1:
+                continue
+
+            # 找左边
+            res1 = 1
+            cur = num
+            while (cur-1) in vals:
+                vals[cur-1] = res1 + 1
+                cur = cur - 1
+                res1 = res1 + 1
+
+            # 找右边
+            res2 = 1
+            cur = num
+            while (cur+1) in vals:
+                vals[num+1] = res2+1
+                cur = cur + 1
+                res2 = res2 +1
+
+            # 注意要把左右两个方向的结果相加
+            vals[num] = res1 + res2 - 1
+
+        return max(vals.values())
+
+nums = [100, 4, 200, 1, 3, 2]
+# nums = [-1,1,0]
+# nums = [1,2,0,1]
+print(Solution().longestConsecutive(nums))
